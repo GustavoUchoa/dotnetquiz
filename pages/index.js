c3c363f6c1b1890821ed16520e0fba95d888b7f8
GustavoUchoa <github@gustavouchoa.net>
2021-01-27 22:54:54 -0300

    parte do código da terceira aula

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -17,7 +19,7 @@ import GitHubCorner from '../src/components/GitHubCorner';
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -46,27 +48,22 @@ export default function Home() {
 
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (e) {
-              e.preventDefault();
-              router.push(`/quiz?name=${name}`);
-            }}
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                router.push(`/quiz?name=${name}`);
+              }}
             >
-              <div>
-                <input
-                  onChange={function (e) {
-                    setName(e.target.value);
-                  }}
-                  placeholder="Qual o seu nome?"
-                />
-              </div>
-              <br />
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Qual o seu nome?"
+                name="nomeUsuario"
+                value={name}
+              />
 
-              <div>
-                <button type="submit" disabled={name.length === 0}>
-                &ensp; Jogar &ensp;
-                  {name}
-                </button>
-              </div>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,83 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function QuizPage() {
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
+
+function LoadingWidget() {
   return (
-    <div>
-      Página do Quiz
-    </div>
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  questionIndex,
+  totalQuestions,
+}) {
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions} `}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <Button>
+          Confirmar
+        </Button>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+export default function QuizPage() {
+  const totalQuestions = db.questions.length;
+  const questionIndex = 0;
+  const question = db.questions[questionIndex];
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <QuestionWidget
+          question={question}
+          questionIndex={questionIndex}
+          totalQuestions={totalQuestions}
+        />
+        <LoadingWidget />
+      </QuizContainer>
+    </QuizBackground>
   );
 }
